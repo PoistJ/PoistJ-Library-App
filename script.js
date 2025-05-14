45482a64e0e6ff2eeefe1faa8c5ae8a59bc74e2b
PoistJ <157406020+PoistJ@users.noreply.github.com>
2025-05-14 03:17:03 -0400

    Changed library layout to grid, added delete button to each "book", changed the sidebar toggle to allow the library div to fill the space when hidden.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const myLibrary = [];
-
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("'New' operator must be used to call this.");
     }
@@ -9,6 +7,11 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
     this.bookID = crypto.randomUUID();
+    this.readStatus = read;
+}
+
+Book.changeRead = function() {
+    this.readStatus = true;
 }
 
 function addBook(form) {
@@ -16,37 +19,42 @@ function addBook(form) {
         return false;
     }
 
-    const newBook = new Book(form.title.value, form.author.value, form.pages.value);
+    const newBook = new Book(form.title.value, form.author.value, form.pages.value, form.read.value);
     myLibrary.push(newBook);
     event.preventDefault();
+   
     var div = document.createElement("div");
-    div.style.width = "400px";
-    div.style.height = "200px";
+    var btn = document.createElement("button");
+    btn.innerHTML = "x";
+    btn.id = newBook.bookID;
+    btn.onclick = deleteButton;
     div.style.backgroundColor = "#e9c46a";
     div.style.color = "#023047";
     div.style.textAlign = "center";
-    div.innerHTML = "<strong>" + form.title.value + "</strong><br>" + form.author.value + "<br>" + form.pages.value + " pages"
+    div.innerHTML = "<strong>" + form.title.value + "</strong><br>" + form.author.value + "<br>" + form.pages.value + 
+    " pages<br>Status: " + form.read.value;
     div.style.borderStyle = "solid";
     div.style.margin = "10px";
     div.style.padding= "10px";
     div.style.borderRadius = "12px";
     div.style.fontSize = "1.2em";
+    div.id = newBook.bookID;
+    
     document.getElementsByClassName("library")[0].appendChild(div);
     document.getElementById("bookInfo").reset();
+    div.appendChild(btn);
 }
 
 function displayLibrary(myLibrary) {
     myLibrary.forEach(Book => {
         var div = document.createElement("div");
-        div.style.width = "400px";
-        div.style.height = "200px";
         div.style.backgroundColor = "#e9c46a";
         div.style.color = "#023047";
         div.style.textAlign = "center";
         div.innerHTML = "<strong>" + Book.title + "</strong><br>" + Book.author + "<br>" + Book.pages + " pages"
         div.style.borderStyle = "solid";
         div.style.margin = "10px";
-        div.style.padding= "10px";
+        div.style.padding = "10px";
         div.style.borderRadius = "12px";
         div.style.fontSize = "1.2em";
         document.getElementsByClassName("library")[0].appendChild(div);
@@ -55,6 +63,7 @@ function displayLibrary(myLibrary) {
 
 function openSidebar() {
     var sidebar = document.getElementsByClassName("sidebar")[0];
+    var library = document.getElementsByClassName("library")[0];
     if (sidebar.style.display === "block") {
         sidebar.style.display = "none";
     } else {
@@ -62,4 +71,12 @@ function openSidebar() {
     }
 }
 
+function deleteButton() {
+    var indexNum = myLibrary.findIndex(i => i.bookID == this.id)
+    myLibrary.splice(indexNum, 1);
+    const bookDiv = document.getElementById(this.id);
+    bookDiv.remove();
+}
+
+const myLibrary = [];
 displayLibrary(myLibrary);
